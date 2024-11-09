fff209bfce0a723a95f29a14ed71bc603d4c23d9
Tim Ruser <tim@ruser.dev>
2024-11-09 15:06:37 +0100

    restructured script

--- script.py
+++ script.py
@@ -90,6 +90,276 @@ def showSignatures():
 
     print(tabulate(showList, headers=["Name", "Max Length", "Chunk size", "Is collection", "Files used"]))
     
+def generate(args, path):
+    
+    if not path.is_dir():
+        print("Input has to be a directory to create signatures")
+    else:
+        
+        dirList = os.listdir(path)
+
+        counter = 0
+
+        if not args.family:
+
+            if args.collection:
+                signatureCollection = []
+
+            for file in dirList:
+                
+                counter += 1
+
+                signatureLength = 0
+                signature = []
+
+                with open(path / file, 'rb') as f:
+                    byte_data = f.read()
+
+                    total_bytes = len(byte_data)
+
+                    fullEntropy = calculateEntropy(byte_data)
+
+                    if fullEntropy > 7.5:
+                        print("Skipped " + file + " because it's probably packed")
+                    else:
+                        numOfChunks = math.ceil(total_bytes / args.chunk_size)
+
+                        f.seek(0)
+
+                        for chunkIterator in range(numOfChunks):
+                            chunk = f.read(args.chunk_size)
+                            
+                            if not chunk:
+                                break
+
+                            chunkEntropy = calculateEntropy(chunk)
+
+                            signature.append(chunkEntropy) 
+
+                        print(str(counter) + "/" + str(len(dirList)) + " : " + str(numOfChunks) + " chunks")
+
+                        if not args.collection:
+
+                            os.makedirs('signatures/', exist_ok=True)
+
+                            with open("signatures/" + file.split('.')[0] + ".json", 'w') as f2:
+                                json.dump({
+                                    "name": file,
+                                    "max-length": numOfChunks,
+                                    "collection": False,
+                                    "chunk-size": args.chunk_size,
+                                    "signature": signature,
+                                    "usedFiles": file
+                                }, f2)
+
+                        else:
+                            signatureCollection.append(signature)
+
+            if args.collection:
+
+                os.makedirs('signatures/', exist_ok=True)
+
+                with open("signatures/" + path.name + ".json", 'w') as f2:
+                    json.dump({
+                        "name": path.name,
+                        "max-length": max([len(sig) for sig in signatureCollection]),
+                        "chunk-size": args.chunk_size,
+                        "collection": True,
+                        "signature": signatureCollection,
+                        "usedFiles": dirList
+                    }, f2)
+
+        else:
+
+            signatures = []
+            longestChunkLength = 0
+
+            for file in dirList:
+                
+                counter += 1
+
+                signatureLength = 0
+                signature = []
+
+                with open(path / file, 'rb') as f:
+                    byte_data = f.read()
+
+                    total_bytes = len(byte_data)
+                    
+                    fullEntropy = calculateEntropy(byte_data)
+
+                    if fullEntropy > 7.8:
+                        print("Skipped " + file + " because it's probably packed")
+                    else:
+                        numOfChunks = math.ceil(total_bytes / args.chunk_size)
+
+                        f.seek(0)
+
+                        for chunkIterator in range(numOfChunks):
+                            chunk = f.read(args.chunk_size)
+                            
+                            if not chunk:
+                                break
+
+                            chunkEntropy = calculateEntropy(chunk)
+
+                            signature.append(chunkEntropy) 
+
+                        print(str(counter) + "/" + str(len(dirList)) + " : " + str(numOfChunks) + " chunks")
+
+                        longestChunkLength = max(longestChunkLength, numOfChunks)
+
+                        signatures.append(signature)
+
+            maxLen = max([len(list) for list in signatures])
+
+            averageSignature = []
+
+            for i in range(maxLen):
+
+                sum = 0
+
+                for sig in signatures:
+                    
+                    if not i > len(sig) - 1:
+                        sum += sig[i]
+                    
+                averageSignature.append(sum / len(signatures))
+
+            os.makedirs('signatures/', exist_ok=True)
+
+            with open("signatures/" + path.name + ".json", 'w') as f2:
+                json.dump({
+                    "name": path.name,
+                    "max-length": longestChunkLength,
+                    "collection": False,
+                    "chunk-size": args.chunk_size,
+                    "signature": averageSignature,
+                    "usedFiles": dirList
+                }, f2)
+
+def test(args, path):
+    
+    with open(path, "rb") as f:
+            
+        if not path.exists():
+            print("The file doesn't exist") 
+            return None
+        if path.is_dir():
+            print("Path can't be a directory")
+            return None
+
+        # --- Calculating test file entropy
+
+        byte_data = f.read()
+
+        total_bytes = len(byte_data)
+
+        fullEntropy = calculateEntropy(byte_data)
+
+        if fullEntropy > 7.8:
+            print("File is probably packed and should therefore be treated with caution")
+        else:
+            
+            f.seek(0)
+
+            numOfChunks = math.ceil(total_bytes / args.chunk_size)
+
+            testSignature = []
+
+            for chunkIterator in range(numOfChunks):
+                        chunk = f.read(args.chunk_size)
+                        
+                        if not chunk:
+                            break
+
+                        chunkEntropy = calculateEntropy(chunk)
+
+                        testSignature.append(chunkEntropy)
+
+            
+            # --- Comparing with signatures ---
+            
+            dirList = os.listdir(os.getcwd() + "/signatures")
+
+            counter = 0
+
+            matchList = []
+
+            for file in dirList:
+            
+                with open("signatures/" + file, "r") as f2:
+
+                    signatureData = json.load(f2)
+
+                    signature = signatureData['signature']
+
+                    if not signatureData['chunk-size'] == args.chunk_size:
+                        print("Signature has the chunk size " + str(signatureData['chunk-size']) + " but " + str(args.chunk_size) + " was given")
+                        return None
+
+                    numOfChunks = signatureData['max-length']
+
+                    cumulatedError = 0
+
+                    if signatureData['collection']:
+
+                        maxLength = numOfChunks
+
+                        cumMatch = 0
+                        numMatches = 0
+                        maxMatch = 0
+
+                        for i in range(len(signature)):
+
+                            cumulatedError = 0
+
+                            for k in range(maxLength):
+
+                                if len(signature[i]) - 1 < k or len(testSignature) - 1 < k:
+                                    cumulatedError += 1
+                                else:
+                                    cumulatedError += abs(testSignature[k] - signature[i][k]) / 8
+
+                            match = 1 - (1 / maxLength) * cumulatedError
+                            cumMatch += match
+                            
+                            maxMatch = max(maxMatch, match)
+
+                            if match > 0.85:
+                                numMatches += 1 
+                        
+                        cumMatch = (1 / len(signature) * cumMatch)
+
+                        matchList.append([
+                            True,
+                            signatureData['name'],
+                            cumMatch,
+                            numMatches,
+                            maxMatch
+                        ])
+
+                    else:
+
+                        maxLength = max(numOfChunks, len(testSignature))
+
+                        for i in range(maxLength):
+
+                            if len(signature) - 1 < i or len(testSignature) - 1 < i:
+                                cumulatedError += 1
+                            else:
+                                cumulatedError += abs(testSignature[i] - signature[i]) / 8
+
+                        match = 1 - (1 / maxLength) * cumulatedError
+                        
+                        matchList.append([
+                            False,
+                            signatureData['name'],
+                            match
+                        ])
+            
+            displayMatchList(matchList)
+
 
 def main():
     
@@ -106,278 +376,13 @@ def main():
         return None
 
     if args.mode == 'generate':
-
-        if not path.is_dir():
-            print("Input has to be a directory to create signatures")
-        else:
-            
-            dirList = os.listdir(path)
-
-            counter = 0
-
-            if not args.family:
-
-                if args.collection:
-                    signatureCollection = []
-
-                for file in dirList:
-                    
-                    counter += 1
-
-                    signatureLength = 0
-                    signature = []
-
-                    with open(path / file, 'rb') as f:
-                        byte_data = f.read()
-
-                        total_bytes = len(byte_data)
-
-                        fullEntropy = calculateEntropy(byte_data)
-
-                        if fullEntropy > 7.5:
-                            print("Skipped " + file + " because it's probably packed")
-                        else:
-                            numOfChunks = math.ceil(total_bytes / args.chunk_size)
-
-                            f.seek(0)
-
-                            for chunkIterator in range(numOfChunks):
-                                chunk = f.read(args.chunk_size)
-                                
-                                if not chunk:
-                                    break
-
-                                chunkEntropy = calculateEntropy(chunk)
-
-                                signature.append(chunkEntropy) 
-
-                            print(str(counter) + "/" + str(len(dirList)) + " : " + str(numOfChunks) + " chunks")
-
-                            if not args.collection:
-
-                                os.makedirs('signatures/', exist_ok=True)
-
-                                with open("signatures/" + file.split('.')[0] + ".json", 'w') as f2:
-                                    json.dump({
-                                        "name": file,
-                                        "max-length": numOfChunks,
-                                        "collection": False,
-                                        "chunk-size": args.chunk_size,
-                                        "signature": signature,
-                                        "usedFiles": file
-                                    }, f2)
-
-                            else:
-                                signatureCollection.append(signature)
-
-                if args.collection:
-
-                    os.makedirs('signatures/', exist_ok=True)
-
-                    with open("signatures/" + path.name + ".json", 'w') as f2:
-                        json.dump({
-                            "name": path.name,
-                            "max-length": max([len(sig) for sig in signatureCollection]),
-                            "chunk-size": args.chunk_size,
-                            "collection": True,
-                            "signature": signatureCollection,
-                            "usedFiles": dirList
-                        }, f2)
-
-            else:
-
-                signatures = []
-                longestChunkLength = 0
-
-                for file in dirList:
-                    
-                    counter += 1
-
-                    signatureLength = 0
-                    signature = []
-
-                    with open(path / file, 'rb') as f:
-                        byte_data = f.read()
-
-                        total_bytes = len(byte_data)
-                        
-                        fullEntropy = calculateEntropy(byte_data)
-
-                        if fullEntropy > 7.8:
-                            print("Skipped " + file + " because it's probably packed")
-                        else:
-                            numOfChunks = math.ceil(total_bytes / args.chunk_size)
-
-                            f.seek(0)
-
-                            for chunkIterator in range(numOfChunks):
-                                chunk = f.read(args.chunk_size)
-                                
-                                if not chunk:
-                                    break
-
-                                chunkEntropy = calculateEntropy(chunk)
-
-                                signature.append(chunkEntropy) 
-
-                            print(str(counter) + "/" + str(len(dirList)) + " : " + str(numOfChunks) + " chunks")
-
-                            longestChunkLength = max(longestChunkLength, numOfChunks)
-
-                            signatures.append(signature)
-
-                maxLen = max([len(list) for list in signatures])
-
-                averageSignature = []
-
-                for i in range(maxLen):
-
-                    sum = 0
-
-                    for sig in signatures:
-                        
-                        if not i > len(sig) - 1:
-                            sum += sig[i]
-                        
-                    averageSignature.append(sum / len(signatures))
-
-                os.makedirs('signatures/', exist_ok=True)
-
-                with open("signatures/" + path.name + ".json", 'w') as f2:
-                    json.dump({
-                        "name": path.name,
-                        "max-length": longestChunkLength,
-                        "collection": False,
-                        "chunk-size": args.chunk_size,
-                        "signature": averageSignature,
-                        "usedFiles": dirList
-                    }, f2)
-
-
-                
-
+        generate(args, path)
+    elif args.mode == 'test':
+        test(args, path)
     else:
+        print("There is no mode named " + args.mode)
 
-        with open(path, "rb") as f:
-            
-            if not path.exists():
-                print("The file doesn't exist") 
-                return None
-            if path.is_dir():
-                print("Path can't be a directory")
-                return None
-
-            # --- Calculating test file entropy
-
-            byte_data = f.read()
-
-            total_bytes = len(byte_data)
-
-            fullEntropy = calculateEntropy(byte_data)
-
-            if fullEntropy > 7.8:
-                print("File is probably packed and should therefore be treated with caution")
-            else:
-                
-                f.seek(0)
-
-                numOfChunks = math.ceil(total_bytes / args.chunk_size)
-
-                testSignature = []
-
-                for chunkIterator in range(numOfChunks):
-                            chunk = f.read(args.chunk_size)
-                            
-                            if not chunk:
-                                break
-
-                            chunkEntropy = calculateEntropy(chunk)
-
-                            testSignature.append(chunkEntropy)
-
-                
-                # --- Comparing with signatures ---
-                
-                dirList = os.listdir(os.getcwd() + "/signatures")
-
-                counter = 0
-
-                matchList = []
-
-                for file in dirList:
-                
-                    with open("signatures/" + file, "r") as f2:
-
-                        signatureData = json.load(f2)
-
-                        signature = signatureData['signature']
-
-                        if not signatureData['chunk-size'] == args.chunk_size:
-                            print("Signature has the chunk size " + str(signatureData['chunk-size']) + " but " + str(args.chunk_size) + " was given")
-                            return None
-
-                        numOfChunks = signatureData['max-length']
-
-                        cumulatedError = 0
-
-                        if signatureData['collection']:
-
-                            maxLength = numOfChunks
-
-                            cumMatch = 0
-                            numMatches = 0
-                            maxMatch = 0
-
-                            for i in range(len(signature)):
-
-                                cumulatedError = 0
-
-                                for k in range(maxLength):
-
-                                    if len(signature[i]) - 1 < k or len(testSignature) - 1 < k:
-                                        cumulatedError += 1
-                                    else:
-                                        cumulatedError += abs(testSignature[k] - signature[i][k]) / 8
-
-                                match = 1 - (1 / maxLength) * cumulatedError
-                                cumMatch += match
-                                
-                                maxMatch = max(maxMatch, match)
-
-                                if match > 0.85:
-                                    numMatches += 1 
-                            
-                            cumMatch = (1 / len(signature) * cumMatch)
-
-                            matchList.append([
-                                True,
-                                signatureData['name'],
-                                cumMatch,
-                                numMatches,
-                                maxMatch
-                            ])
-
-                        else:
-
-                            maxLength = max(numOfChunks, len(testSignature))
-
-                            for i in range(maxLength):
-
-                                if len(signature) - 1 < i or len(testSignature) - 1 < i:
-                                    cumulatedError += 1
-                                else:
-                                    cumulatedError += abs(testSignature[i] - signature[i]) / 8
-
-                            match = 1 - (1 / maxLength) * cumulatedError
-                            
-                            matchList.append([
-                                False,
-                                signatureData['name'],
-                                match
-                            ])
-                
-                displayMatchList(matchList)
-    
+    return None 
 
 if __name__ == "__main__":
     exit(main())
